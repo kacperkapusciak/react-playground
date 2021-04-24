c4cac50f86a1d6808221a66bfaca4f1e031d6cc7
kacperkapusciak <kapusciak.k@gmail.com>
2021-04-24 19:09:06 +0200

    feat: implement ConditionalRendering example

--- src/examples/ConditionalRendering.js
+++ src/examples/ConditionalRendering.js
@@ -1,5 +1,65 @@
+import { useState } from 'react';
+
+const Circle = () => (
+  <div
+    style={{
+      width: 100,
+      height: 100,
+      border: '1px solid transparent',
+      backgroundColor: 'red',
+      borderRadius: '50%',
+    }}
+  />
+);
+
+const Square = () => (
+  <div
+    style={{
+      width: 100,
+      height: 100,
+      border: '1px solid transparent',
+      backgroundColor: 'green',
+    }}
+  />
+);
+
 function App() {
-  return <div>ConditionalRendering</div>;
+  const [shape, setShape] = useState('circle');
+
+  const handleChangeShape = () => {
+    setShape((prevShape) => setShape(prevShape === 'circle' ? 'square' : 'circle'));
+  };
+
+  return (
+    <div style={styles.container}>
+      <button style={styles.button} onClick={handleChangeShape}>
+        Zmień figurę
+      </button>
+      {/* conditional rendering */}
+      {shape === 'circle' ? <Circle /> : <Square />}
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  counter: {
+    fontWeight: 'bold',
+    fontSize: 28,
+  },
+  button: {
+    height: 40,
+    width: 200,
+    border: '1px solid transparent',
+    borderRadius: 5,
+    margin: 8,
+    backgroundColor: 'dodgerblue',
+    color: 'white',
+  },
+};
 
 export default App;
