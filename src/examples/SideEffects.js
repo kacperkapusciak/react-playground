4b34651e8d6e465039a067624b4f68e5bec5722e
kacperkapusciak <kapusciak.k@gmail.com>
2021-05-16 11:02:21 +0200

    feat: remake sifeeffects example

--- src/examples/SideEffects.js
+++ src/examples/SideEffects.js
@@ -1,28 +1,22 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/albums');
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [name, setName] = useState(window.localStorage.getItem('name') || '');
 
   useEffect(() => {
-    getData();
-  }, []);
+    window.localStorage.setItem('name', name);
+  });
+
+  const handleChange = (event) => setName(event.target.value);
 
   return (
-    <ol>
-      {data.map((el) => (
-        <li key={el.id}>{el.title}</li>
-      ))}
-    </ol>
+    <>
+      <form>
+        <div>Twoje imię: </div>
+        <input value={name} id="name" onChange={handleChange} />
+      </form>
+      {name ? `Cześć, ${name}` : 'Wpisz swoje imię'}
+    </>
   );
 }
 
